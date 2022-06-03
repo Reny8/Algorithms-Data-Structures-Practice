0265d10bc7c3db57434c0c73e08bfc409058cfe0
Reny Reyes <renyreyes8@hotmail.com>
2022-06-03 14:29:08 -0400

    Space Complexity example function

---
+++ main.py
@@ -0,0 +1,7 @@
+# Collect the first and last post
+posts = ['hello, Just signed up!', 'Just watched a movie', 'Will be watching the new show tonight']
+def first_and_last(list):
+    print(list[1])
+    print(list[-1])
+    
+first_and_last(posts)
--- practice.js
+++ practice.js
@@ -1,5 +1,5 @@
 // Function to log all pairs of array
-// O(n ** 2) - Quadratic Time
+// O(n^2) - Quadratic Time VERY SLOW NOT THE BEST METHOD
 const boxes = ['a','b','c','d','e']
 function logAllPairsOfArray(array) {
  for (let index = 0; index < array.length; index++) {
@@ -8,4 +8,9 @@ function logAllPairsOfArray(array) {
     }
  }
 }
-logAllPairsOfArray(boxes)
+// logAllPairsOfArray(boxes)
+
+// Fins 1st tweet, Find last tweet
+const posts = ['hello, Just signed up!', 'Just watched a movie', 'Will be watching the new show tonight']
+console.log(posts[0])
+console.log(posts[posts.length-1])
