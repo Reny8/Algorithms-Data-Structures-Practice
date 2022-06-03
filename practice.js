d509988e1a50a564e23475983925b15d46caaa2e
Reny Reyes <renyreyes8@hotmail.com>
2022-06-03 18:33:28 -0400

    compare items between arrays in js

--- practice.js
+++ practice.js
@@ -23,8 +23,26 @@ function firstAndLast(array) {
 }
 // firstAndLast(posts);
 
+
 // Given 2 arrays, create a function that let's a user know whether these two arrays contain any common items
 // arrays with no size limit
+const letters1 = ["a", "b", "c", "x"];
+const letters2 = ["z", "y", "a"];
 function commonItem(firstArray, secondArray) {
-// Set up
+  let object = {};
+  // loop through first array and create object where properities === items
+  for (let index = 0; index < firstArray.length; index++) {
+    if (!object[firstArray[index]]) {
+      const item = firstArray[index];
+      object[item] = true;
     }
+  }
+  // loop through second array and check if item in second array exists on obj
+  for (let index = 0; index < secondArray.length; index++) {
+    if (object[secondArray[index]]) {
+      return true;
+    }
+  }
+return false
+}
+console.log(commonItem(letters1, letters2));
