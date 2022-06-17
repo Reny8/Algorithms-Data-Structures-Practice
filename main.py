7cf6306226612ee21097eafed613b0357daa6644
Reny Reyes <renyreyes8@hotmail.com>
2022-06-17 15:27:39 -0400

    Added a function to return indices of sum = target

--- main.py
+++ main.py
@@ -102,4 +102,23 @@ def merge_arrays(first_array, second_array):
         first_array.append(item)
     first_array.sort()
     print(first_array)
-merge_arrays([0,1,2,3,4], [16,23,30])
+# merge_arrays([0,1,2,3,4], [16,23,30])
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+# Example 1:
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
+def target_sum(numbers_array, target):
+    index_sums = []
+    for number in numbers_array:
+        if (target - number) in numbers_array:
+            index_sums.append(numbers_array.index(number))
+            index_sums.append(numbers_array.index(target - number))
+            break     
+    return index_sums
+
+sum_list = target_sum([2,7,11,15], 9)
+print(sum_list)
