858991892c880bed9f9558d31cb18727fd94e425
Reny Reyes <renyreyes8@hotmail.com>
2022-07-05 14:04:36 -0400

    Created the template for a stack class

---
+++ stacksAndQueues.js
@@ -0,0 +1,18 @@
+//Stacks
+// LIFO
+//top and a bottom which can be built with linked lists or arrays
+//building from arrays is not efficient
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {}
+  push(value) {}
+  pop() {}
+}
+
+//Queues
+// FIFO
+// building with inked lists
