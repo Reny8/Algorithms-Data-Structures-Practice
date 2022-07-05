fbb775c6950e207e3bbea35e57ddfe6aab1905f2
Reny Reyes <renyreyes8@hotmail.com>
2022-07-05 10:48:32 -0400

    Created the constructor for the doublylinkedlist class

---
+++ doublyLinkedList.js
@@ -0,0 +1,11 @@
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
