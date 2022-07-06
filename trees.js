eaf0328a8ff6ff44be20e4200ba1a5fba4c10335
Reny Reyes <renyreyes8@hotmail.com>
2022-07-06 15:06:59 -0400

    Created a lookup function for binary search tree class

--- trees.js
+++ trees.js
@@ -39,17 +39,35 @@ class BinarySearchTree {
       }
     }
   }
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+    return false;
+  }
 }
 const tree = new BinarySearchTree();
 tree.insert(9);
-tree.insert(4)
-tree.insert(6)
-// tree.insert(20)
-// tree.insert(170)
-// tree.insert(15)
-// tree.insert(1)
-console.log(JSON.stringify(traverse(tree.root)));
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+console.log(tree.lookup(9))
+console.log(tree.lookup(90))
+// console.log(JSON.stringify(traverse(tree.root)));
 
 function traverse(node) {
   const tree = { value: node.value };
