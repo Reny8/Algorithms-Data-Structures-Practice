9a7543a71217799f6ed0bae27bbd44634621bbd5
Reny Reyes <renyreyes8@hotmail.com>
2022-07-06 14:37:41 -0400

    Created a template to create a binary search tree class

--- trees.js
+++ trees.js
@@ -6,3 +6,12 @@ class binaryTreeNode {
     this.right = null;
   }
 }
+// Level 0: 2^0 = 1
+// Level 1: 2^1 = 2
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {}
+  lookup(value) {}
+}
