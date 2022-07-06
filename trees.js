b986d22d43e8a8f907dbebddb9ab55bd07e87859
Reny Reyes <renyreyes8@hotmail.com>
2022-07-06 15:00:14 -0400

    Created an insert function for the binary search tree

--- trees.js
+++ trees.js
@@ -13,26 +13,47 @@ class BinarySearchTree {
     this.root = null;
   }
   insert(value) {
-
+    const newNode = new binaryTreeNode(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          //INSERT THE NUMBER TO LEFT
+          if (!currentNode.left) {
+            // If there is not a value in the left
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // INSERT THE NUMBER TO RIGHT
+          if (!currentNode.right) {
+            // If there is not a value in the right
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
   }
   lookup(value) {}
 }
-const tree = new BinarySearchTree()
-// tree.insert(9)
-// tree.insert(4)
-// tree.insert(6)
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4)
+tree.insert(6)
 // tree.insert(20)
 // tree.insert(170)
 // tree.insert(15)
 // tree.insert(1)
-JSON.stringify(traverse(tree.root))
-
+console.log(JSON.stringify(traverse(tree.root)));
 
 function traverse(node) {
-    const tree = {value: node.value};
-    tree.left = node.left === null ? null :
-    traverse(node.left);
-    tree.right = node.right === null ? null :
-    traverse(node.right);
-    return tree
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
 }
