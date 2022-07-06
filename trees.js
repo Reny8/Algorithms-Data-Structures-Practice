012ec60d8b954e50d764df2c44004bcb1e804521
Reny Reyes <renyreyes8@hotmail.com>
2022-07-06 14:43:38 -0400

    Added function to test the binary search tree insert results

--- trees.js
+++ trees.js
@@ -12,6 +12,27 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(value) {}
+  insert(value) {
+
+  }
   lookup(value) {}
+}
+const tree = new BinarySearchTree()
+// tree.insert(9)
+// tree.insert(4)
+// tree.insert(6)
+// tree.insert(20)
+// tree.insert(170)
+// tree.insert(15)
+// tree.insert(1)
+JSON.stringify(traverse(tree.root))
+
+
+function traverse(node) {
+    const tree = {value: node.value};
+    tree.left = node.left === null ? null :
+    traverse(node.left);
+    tree.right = node.right === null ? null :
+    traverse(node.right);
+    return tree
 }
