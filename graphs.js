637b970e57f47471868eb6c7b9008dc1733ab90d
Reny Reyes <renyreyes8@hotmail.com>
2022-07-13 15:03:06 -0400

    Created functionality for adding vertex to the graph

--- graphs.js
+++ graphs.js
@@ -25,9 +25,13 @@ class Graph {
     this.numberOfNodes = 0;
     this.adjacentList = {};
   }
-  addVertex(node) {}
+  addVertex(node) {
+    this.adjacentList[node] = []
+    this.numberOfNodes++
+  }
   addEdge(node1, node2) {
-    //undirected Graph
+    //undirected Graph: node1 and node2 point to each other
+
   }
   showConnections() {
     const allNodes = Object.keys(this.adjacentList);
@@ -42,3 +46,21 @@ class Graph {
     }
   }
 }
+const myGraph = new Graph()
+myGraph.addVertex("0")
+// myGraph.addVertex("1")
+// myGraph.addVertex("2")
+// myGraph.addVertex("3")
+// myGraph.addVertex("4")
+// myGraph.addVertex("5")
+// myGraph.addVertex("6")
+// myGraph.addEdge("3","1")
+// myGraph.addEdge("3","4")
+// myGraph.addEdge("4","2")
+// myGraph.addEdge("4","5")
+// myGraph.addEdge("1","2")
+// myGraph.addEdge("1","0")
+// myGraph.addEdge("0","2")
+// myGraph.addEdge("6","5")
+
+myGraph.showConnections()
