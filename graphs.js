0512b15cc59fcedceed1df7b3700b9edca168d3d
Reny Reyes <renyreyes8@hotmail.com>
2022-07-13 14:44:47 -0400

    Created an adjacent matrix

--- graphs.js
+++ graphs.js
@@ -1,6 +1,20 @@
-//EDGE LIST 
-const edgeGraph = [[0,2], [2,3], [2,1], [1,3]]
+//EDGE LIST
+const edgeGraph = [
+  [0, 2],
+  [2, 3],
+  [2, 1],
+  [1, 3],
+];
 
-// ADJACENT LIST 
+// ADJACENT LIST
 // showing connections of each node in the graph
-const adjacentGraph = [[2], [2,3], [0,1,3], [1,2]] 
+const adjacentGraph = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// ADJACENT MATRIX]
+// first index is connected with the values 1 index positions
+const adjacentMatrixGraph = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0],
+];
