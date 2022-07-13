e9f963ac3c5d1cd67e7edcb11df2c0529060469f
Reny Reyes <renyreyes8@hotmail.com>
2022-07-13 14:40:53 -0400

    Created adjacent list

--- graphs.js
+++ graphs.js
@@ -1,2 +1,6 @@
 //EDGE LIST 
-const graph = [[0,2], [2,3], [2,1], [1,3]]
+const edgeGraph = [[0,2], [2,3], [2,1], [1,3]]
+
+// ADJACENT LIST 
+// showing connections of each node in the graph
+const adjacentGraph = [[2], [2,3], [0,1,3], [1,2]] 
