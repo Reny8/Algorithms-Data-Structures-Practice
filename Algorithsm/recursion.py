f650882201e46a9bd0e4281eafb6c69494f732c1
Reny Reyes <renyreyes8@hotmail.com>
2022-07-13 17:05:57 -0400

    Created a recursive function to find factorials

--- Algorithsm/recursion.py
+++ Algorithsm/recursion.py
@@ -7,7 +7,23 @@ def inception(counter):
     counter += 1
     print(counter)
     return inception(counter)
+# times = 0
+# inception(times)
 
-times = 0
-inception(times)
+# WRITE TWO FUNCTIONS THAT FIND THE FACTORIAL OF ANY NUMBER.
+# ONE SHOULD USE RECURSIVE, THE OTHER SHOULD JUST USE A FOR LOOP
 
+def find_factorial_recursive(number,answer=1):
+    if number == 1:
+        return print(answer)
+    answer *= number
+    number -= 1
+    return find_factorial_recursive(number,answer)
+find_factorial_recursive(2)
+
+def find_factorial_iterative(number):
+    answer = number
+    for num in range((number-1), 1,-1):
+        answer *= num
+    return print(answer)
+# find_factorial_iterative(5)
