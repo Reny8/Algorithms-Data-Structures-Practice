75b17c8addff754731c93cf140d39ad390cae54c
Reny Reyes <renyreyes8@hotmail.com>
2022-07-13 16:25:01 -0400

    Added comments for the recursive function

--- Algorithsm/recursion.py
+++ Algorithsm/recursion.py
@@ -1,10 +1,13 @@
-
+# IDENTFY BASE CASE
+# IDENTIFY THE RECURSIVE CASE
+# RETURN WHEN NEEDED USUALLY WITH TWO RETURNS 
 def inception(counter):
     if counter == 3:
         return print("Done")
     counter += 1
     print(counter)
-    inception(counter)
+    return inception(counter)
 
 times = 0
 inception(times)
+
