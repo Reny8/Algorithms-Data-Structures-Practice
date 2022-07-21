cb3b8e16f541713756f3396cbef5203d4767f841
Reny Reyes <renyreyes8@hotmail.com>
2022-07-21 11:53:59 -0400

    Added a note to return to the binary search tree file

--- Algorithms/searching/searching_bfs_dfs.py
+++ Algorithms/searching/searching_bfs_dfs.py
@@ -2,5 +2,7 @@ beasts = ['Centaur', 'Godzilla', 'Mosura', 'Minotaur']
 # DIFFERENT WAYS TO SEARCH 
 
 # FINDS INDEX USING LINEAR SEARCH
-found_index = beasts.index('Godzilla')
-print(found_index)
+# found_index = beasts.index('Godzilla')
+# print(found_index)
+
+# FUNCTIONALITY LOCATED IN THE BINARY SEARCH TREE DATA STRUCTURE FILE
